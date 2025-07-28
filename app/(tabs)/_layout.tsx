52f4e4abf132484e9403dc642f2defff9da4f441
Greatteacherjubro <wisdomjh100@gmail.com>
2025-07-28 16:31:56 +0900

    feat: web view

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,11 +1,53 @@
 import { Tabs } from "expo-router";
 import React from "react";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+
+const HomeIcon = ({
+  focused,
+  color,
+  size,
+}: {
+  focused: boolean;
+  color: string;
+  size: number;
+}) => {
+  const iconName = focused ? "home" : "home-outline";
+  return <MaterialCommunityIcons name={iconName} color={color} size={26} />;
+};
+
+const ShopIcon = ({
+  focused,
+  color,
+  size,
+}: {
+  focused: boolean;
+  color: string;
+  size: number;
+}) => {
+  const iconName = focused ? "shopping" : "shopping-outline";
+  return <MaterialCommunityIcons name={iconName} color={color} size={26} />;
+};
 
 export default function TabLayout() {
   return (
-    <Tabs>
-      <Tabs.Screen name="index" />
-      <Tabs.Screen name="shopping" />
+    <Tabs
+      screenOptions={{
+        tabBarStyle: {
+          backgroundColor: "black",
+        },
+        tabBarActiveTintColor: "white",
+        tabBarInactiveTintColor: "white",
+        headerShown: false,
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{ tabBarLabel: "홈", tabBarIcon: HomeIcon }}
+      />
+      <Tabs.Screen
+        name="shopping"
+        options={{ tabBarLabel: "쇼핑", tabBarIcon: ShopIcon }}
+      />
     </Tabs>
   );
 }
